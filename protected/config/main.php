37d88c52f487a18f0964e6185927b7187b3a103f
«Gregurco» <gregurco.vlad@gmail.com>
2014-03-23 01:31:28 +0200

    test

--- protected/config/main.php
+++ protected/config/main.php
@@ -25,7 +25,7 @@ return array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>'Enter Your Password Here',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			'ipFilters'=>array('95.65.77.178','::1'),
 		),
 	),
 
